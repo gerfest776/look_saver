84a1abb1a2a5de497a03b74ddd7ac6e44ea3b637
German <94898610+gerfest776@users.noreply.github.com>
2022-02-23 13:37:03 +0300

    fetch: gunicorn nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,12 @@ echo "Waiting for postgres..."
 while ! nc -z database 5432; do
   sleep 0.1
 done
+
+echo "Collect static files"
+
+echo yes | python manage.py collectstatic
+
+
 echo "PostgreSQL started"
 
 python manage.py migrate
